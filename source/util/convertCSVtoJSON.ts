4114519ba7892d43d03e6796d46081cf03fbffb8
Raffael Schaefer <69517972+RaffaelSchaefer@users.noreply.github.com>
2021-11-19 15:11:50 +0100

    stuff

--- source/util/convertCSVtoJSON.ts
+++ source/util/convertCSVtoJSON.ts
@@ -1,11 +1,27 @@
 const fs = require("fs");
+const readline = require("readline");
 
-const convertCSVtoJSON = (path: string) => {
-    let types: string[] = [];
-    try {
-        const tmp = fs.read(path, "utf-8");
-    } catch (err) {
-        console.log(err);
+const convertCSVtoJSON = async (path: string) => {
+    const fileStream: any = fs.createReadStream(path);
+    const rl: any = readline.createInterface({input: fileStream, crlfDelay: Infinity});
+    let ID: number = -1;
+    let types: string = "";
+    let tmpOBJ: string = "";
+    let out: string = "";
+    for await (const line of rl) {
+        if (ID <= -1) {
+            types = line;
+            types.split(",");
+        } else {
+            tmpOBJ = line;
+            tmpOBJ.split(";");
+            for (let i = 0; i < tmpOBJ.length; i++) {
+                out += types[i]+": "+tmpOBJ[i];
+            }
+            console.log(out);
+            console.log(`Line ${ID} from file: ${line}`);
+        }
+        ID++;
     }
 }
 
