665169b073c2b666a1fb21a461ac0982cca3d34b
Raffael Elias Schäfer <s20a3c@umwelt-campus.de>
2021-11-19 15:56:51 +0100

    File conversion works

--- source/app.ts
+++ source/app.ts
@@ -1,3 +1,3 @@
 const convert  = require("./util/convertCSVtoJSON.js")
 
-convert("./source/db/mineral_data_german_sample.csv").then().catch();
+convert("./source/db/mineral_data_german_211119.csv","source/db","mineral_data_german_211119.json").then().catch();
--- source/util/convertCSVtoJSON.ts
+++ source/util/convertCSVtoJSON.ts
@@ -1,28 +1,11 @@
-const fs = require("fs");
-const readline = require("readline");
+const csv = require('csvtojson');
+const fs = require('fs');
 
-const convertCSVtoJSON = async (path: string) => {
-    const fileStream: any = fs.createReadStream(path);
-    const rl: any = readline.createInterface({input: fileStream, crlfDelay: Infinity});
-    let ID: number = -1;
-    let types: string = "";
-    let tmpOBJ: string = "";
-    let out: string = "";
-    for await (const line of rl) {
-        if (ID <= -1) {
-            types = line;
-            types.split(",");
-        } else {
-            tmpOBJ = line;
-            tmpOBJ.split(";");
-            for (let i = 0; i < tmpOBJ.length; i++) {
-                out += types[i]+": "+tmpOBJ[i];
-            }
-            console.log(out);
-            console.log(`Line ${ID} from file: ${line}`);
-        }
-        ID++;
-    }
+const convertCSVtoJSON = async (path: string, exportDir: string, exportFileName: string) => {
+    csv().fromFile(path).then((jsonObj: any)=>{
+        fs.writeFileSync(exportDir+'/'+exportFileName, JSON.stringify(jsonObj));
+        console.log(exportFileName+" was saved!");
+    });
 }
 
 module.exports = convertCSVtoJSON;
