20fdf25a5dee5c17f2aae563890ad4e6acaddd13
Raffael Elias Schäfer <s20a3c@umwelt-campus.de>
2021-11-20 12:34:35 +0100

    All files are converted

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
 const convert  = require("./util/convertCSVtoJSON.js")
 
 convert("./src/db/csv/mineral_data_german_211119.csv","src/db/json","mineral_data_german_211119.json").then().catch();
+for (let i = 1; i <= 6; i++) {
+    convert("./src/db/csv/mineral_literature_data_german_211119_"+i+".csv","src/db/json","mineral_literature_data_german_211119_"+i+".json").then().catch();
+}
