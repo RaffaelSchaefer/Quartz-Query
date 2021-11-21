3967c07c31c44de4b8a3569c21f09aeb1ecc1b62
Raffael Elias Schäfer <s20a3c@umwelt-campus.de>
2021-11-21 16:03:14 +0100

    updates

--- src/init.ts
+++ src/init.ts
@@ -1,2 +1,8 @@
 const conversion = require("./scripts/util/convertCSVtoJSON");
-conversion("db/csv/mineral_data_german_211119.csv","db/json","mineral_data_german_211119.json").then().catch();
+conversion(
+  "db/csv/mineral_data_german_211119.csv",
+  "db/json",
+  "mineral_data_german_211119.json"
+)
+  .then()
+  .catch();
