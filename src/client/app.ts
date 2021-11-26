840324184d81f5c916d54fb20f715124a81ff758
Raffael Elias Schäfer <raffael1506@gmail.com>
2021-11-26 10:35:46 +0100

    Improvements

--- src/client/app.ts
+++ src/client/app.ts
@@ -1,3 +1,5 @@
 import { buildMineralModule } from "./scripts/module/create/buildMineralModule.js";
 
-buildMineralModule();
+for (let i = 0; i < 10; i++) {
+  buildMineralModule(i);
+}
--- src/client/scripts/module/create/buildMineralModule.ts
+++ src/client/scripts/module/create/buildMineralModule.ts
@@ -11,12 +11,18 @@ export async function buildMineralModule(pos: number = 0) {
 
   const mineral: any = await fetchMineralDB(url, pos);
 
-  const element = document.getElementsByTagName("ul")[0];
+  const element = document.getElementById("content")!;
+  const listElement = document.createElement("ul");
+  const displayElement = document.createElement("div");
+  displayElement.classList.add("displayElement");
 
   for (let i = 0; i < Object.keys(mineral).length; i++) {
     const tagACode = document.createElement("li");
     const textACode = document.createTextNode(`${Object.keys(mineral)[i]}: ${mineral.getByIndex(i)}`);
     tagACode.appendChild(textACode);
-    element.appendChild(tagACode);
+    tagACode.classList.add(`${Object.keys(mineral)[i]}_css`);
+    listElement.appendChild(tagACode);
   }
+  displayElement.appendChild(listElement);
+  element.appendChild(displayElement);
 }
