58bb5790b2de819fb348d6d3d2b43af7faa461fb
Raffael Schaefer <69517972+RaffaelSchaefer@users.noreply.github.com>
2021-11-30 18:00:36 +0100

    Working on search

--- src/client/scripts/app.ts
+++ src/client/scripts/app.ts
@@ -1,16 +1,5 @@
-import { buildMineralModule } from "./module/ui/buildMineralModule.js";
+import {search} from "./module/ui/search";
 
-let scrollIndex: number = 0;
-
-for (let i = 0; i < 10; i++) {
-  buildMineralModule(false, scrollIndex, "list");
-  scrollIndex++;
-}
-
-window.addEventListener("scroll", () => {
-  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-  if (clientHeight + scrollTop >= scrollHeight - 5) {
-    buildMineralModule(false, scrollIndex, "list");
-    scrollIndex++;
-  }
-});
+document.getElementById("inputSubmit")!.addEventListener("click", () => {
+  search(document.getElementById("inputIdentifier")!.innerHTML, document.getElementById("inputKeyword")!.innerHTML);
+})
--- src/client/scripts/module/ui/search.ts
+++ src/client/scripts/module/ui/search.ts
@@ -1,1 +1,23 @@
-export async function search(identifier: string, keyword: string) {}
+// @ts-ignore
+import { buildMineralModule } from "./buildMineralModule.js";
+
+let scrollIndex: number = 0;
+
+/*for (let i = 0; i < 10; i++) {
+  if (scrollIndex <  size.amount) {
+    buildMineralModule(false, scrollIndex, "list");
+    scrollIndex++;
+  }
+}
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (clientHeight + scrollTop >= scrollHeight - 5 && scrollIndex < size.amount) {
+    buildMineralModule(false, scrollIndex, "list");
+    scrollIndex++;
+  }
+});*/
+
+export async function search(identifier: string, keyword: string) {
+
+}
