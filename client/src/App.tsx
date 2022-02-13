b95840530c567df7004d4b4a904cbdfda7584573
Raffael Schaefer <69517972+RaffaelSchaefer@users.noreply.github.com>
2022-02-13 20:45:58 +0100

    Switching to React

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-9xl font-bold text-center text-slate-900 p-6">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
