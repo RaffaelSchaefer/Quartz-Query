47bd7b94a2d3be634224e9bc1d9689a1c8240b40
Raffael Elias Schäfer <raffael1506@gmail.com>
2022-02-22 18:19:36 +0100

    Working on Backend

--- app/components/Result.tsx
+++ app/components/Result.tsx
@@ -4,15 +4,28 @@ import Item from "./Item";
 export default class Result extends React.Component {
 
     state = {
-        loading: true
+        loading: true,
+        query: null
     }
 
-    componentDidMount() {
+    async componentDidMount() {
+        const url = "http://localhost:3000/api/mineral/search/Silber";
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({query: data.results})
     }
 
     render () {
+        // @ts-ignore
         return (
             <div className="w-full h-full flex flex-wrap p-6 overflow-y-auto ease-in-out duration-700">
+                {/*@ts-ignore*/}
+                {this.state.loading || !this.state.query ? (
+                    <p>loading...</p>
+                ) : (
+                    <Item A_Code={this.state.query}/>
+                )
+                }
             </div>
         );
     };
