75c706e014328b4fcc2e46e7d9f7842e9b786ba2
Raffael Elias Schäfer <raffael1506@gmail.com>
2022-02-23 21:58:19 +0100

    adding redux

--- app/components/Result.tsx
+++ app/components/Result.tsx
@@ -1,51 +1,52 @@
-import React, { useEffect, useState } from "react";
-import { Mineral } from "../types/mineral";
+import React, {useEffect, useState} from "react";
+import {Mineral} from "../types/mineral";
 import Item from "./Item";
+import {apiURL} from "../constants/apiConstants";
 
-const url = "http://localhost:3000/api/mineral/search/";
+const url = `${apiURL}mineral/search/`;
 
 export default function Result() {
+    const [loading, setLoading] = useState<boolean>(true);
+    const [minerals, setMinerals] = useState<Mineral[] | undefined>(undefined);
+    const [input, setInput] = useState<string>("Quartz");
 
-  const [loading, setLoading] = useState<boolean>(true);
-  const [minerals, setMinerals] = useState<Mineral[] | undefined>(undefined);
-  const [input, setInput] = useState<string>("Quartz");
+    const wrapperStyle: string = "w-full h-full flex flex-wrap p-6 overflow-y-auto scrollbar-hide ease-in-out duration-700";
 
-  const wrapperStyle: string = "w-full h-full flex flex-wrap p-6 overflow-y-auto scrollbar-hide ease-in-out duration-700";
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(url + input);
+            const data = await response.json();
+            setMinerals(data);
+            setLoading(false);
+        };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(url + input);
-      const data = await response.json();
-      setMinerals(data);
-      setLoading(false);
-    };
+        fetchData().catch(console.error);
+    }, [input]);
 
-    fetchData().catch(console.error);
-  }, [input]);
+    if (loading) {
+        return (
+            <div className={wrapperStyle}>
+                <p className="text-5xl font-bold">LOADING</p>
+            </div>
+        );
+    }
 
-  if (loading) {
+    if (!minerals) {
+        return (
+            <div className={wrapperStyle}>
+                <p className="text-5xl font-bold">ERROR</p>
+            </div>
+        )
+    }
+
     return (
-      <div className={wrapperStyle}>
-        <p className="text-5xl font-bold">LOADING</p>
-      </div>
+        <div className={wrapperStyle}>
+            {minerals.map((mineral) => (
+                <Item A_Code={mineral.A_Code} Name={mineral.Name} Paragenese={mineral.Paragenese}
+                      Fundort={mineral.Funddatum}
+                      Ausbildung={mineral.Ausbildung} Funddatum={mineral.Funddatum} Kaufdatum={mineral.Kaufdatum}
+                      Tauschdatum={mineral.Kaufdatum} Kauf_Tauschpreis={mineral.Kauf_Tauschpreis}
+                      Wert_DM={mineral.Wert_DM}/>))}
+        </div>
     );
-  }
-
-  if (!minerals) {
-    return (
-      <div className={wrapperStyle}>
-        <p className="text-5xl font-bold">ERROR</p>
-      </div>
-    )
-  }
-
-  return (
-    <div className={wrapperStyle}>
-      {minerals.map((mineral) => (
-        <Item A_Code={mineral.A_Code} Name={mineral.Name} Paragenese={mineral.Paragenese} Fundort={mineral.Funddatum}
-              Ausbildung={mineral.Ausbildung} Funddatum={mineral.Funddatum} Kaufdatum={mineral.Kaufdatum}
-              Tauschdatum={mineral.Kaufdatum} Kauf_Tauschpreis={mineral.Kauf_Tauschpreis}
-              Wert_DM={mineral.Wert_DM} />))}
-    </div>
-  );
 }
---
+++ app/constants/apiConstants.ts
@@ -0,0 +1,2 @@
+export const URL: string = "http://localhost:3000";
+export const apiURL: string = `${URL}/api/`;
