bc394950097115e4a0e196ec1c6fe6d1058df101
Raffael Elias Schäfer <s20a3c@umwelt-campus.de>
2022-09-04 15:34:18 +0200

    Working on MineralCreator.vue

--- client/src/components/MineralCreator.vue
+++ client/src/components/MineralCreator.vue
@@ -1,20 +1,25 @@
 <template>
-<div class="z-20 fixed top-0 left-0 h-full w-full grid">
-  <form class="self-end justify-self-end bg-white p-2 m-2 border rounded shadow dark:bg-slate-600" v-if="isVisible" @submit.prevent="console.log('Hallo Welt!')">
+<div class="z-50 lg:z-20 fixed top-0 left-0 h-full w-full grid">
+  <form class="lg:self-end lg:justify-self-end bg-white p-2 lg:m-2 w-full lg:w-fit border lg:rounded lg:shadow dark:bg-slate-600" v-if="isVisible" @submit.prevent="console.log('Hallo Welt!')">
     <div class="flex justify-between">
-      <p class="font-bold">Mineral Erstellen</p>
-      <button @click="isVisible = !isVisible" class="w-5 h-5 rounded-full bg-blue-500 hover:bg-blue-400"></button>
+      <p class="text-2xl lg:text-lg font-bold">Mineral Erstellen</p>
+      <button @click="isVisible = !isVisible" class="w-5 h-5 text-center rounded-full bg-red-500 hover:bg-red-400"></button>
     </div>
     <hr class="my-1">
-    <label class="text-slate-900">A-Code: <input type="text" name="A-Code" required></label><br>
-    <label class="text-slate-900">Name: <input type="text" name="Name" required></label><br>
-    <label class="text-slate-900">Paragenese: <input type="text" name="Paragenese"></label><br>
-    <label class="text-slate-900">Ausbildung: <input type="text" name="Ausbildung"></label><br>
-    <label class="text-slate-900">Funddatum: <input type="date" name="Funddatum" required></label><br>
-    <label class="text-slate-900">Kaufdatum: <input type="date" name="Kaufdatum"></label><br>
-    <label class="text-slate-900">Tauschdatum: <input type="date" name="Tauschdatum"></label><br>
-    <label class="text-slate-900">Kauf/Tauschpreis: <input type="number" name="Kauf_Tauschpreis"></label><br>
-    <label class="text-slate-900">Wert in DM: <input type="number" name="Wert_DM"></label><br>
+    <label class="text-slate-900">A-Code: <input class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" type="text" name="A-Code" required></label><br>
+    <label class="text-slate-900">Name: <input class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" type="text" name="Name" required></label><br>
+    <label class="text-slate-900">Fundort: <input class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" type="text" name="Fundort" required></label><br>
+    <label class="text-slate-900">Paragenese: <input class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" type="text" name="Paragenese"></label><br>
+    <label class="text-slate-900">Ausbildung: <input class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" type="text" name="Ausbildung"></label><br>
+    <div class="flex flex-auto space-x-2 my-2 justify-between">
+      <label class="text-slate-900">Funddatum: <br><input class="h-10 border mt-1 rounded px-4 w-fit bg-gray-50" type="date" name="Funddatum"></label><br>
+      <label class="text-slate-900">Kaufdatum: <br><input class="h-10 border mt-1 rounded px-4 w-fit bg-gray-50" type="date" name="Kaufdatum"></label><br>
+      <label class="text-slate-900">Tauschdatum: <br><input class="h-10 border mt-1 rounded px-4 w-fit bg-gray-50" type="date" name="Tauschdatum"></label><br>
+    </div>
+    <div class="flex flex-auto space-x-2 my-2">
+      <label class="text-slate-900">Kauf/Tauschpreis: <br><input class="h-10 border mt-1 rounded px-4 w-fit bg-gray-50" type="number" name="Kauf_Tauschpreis"></label><br>
+      <label class="text-slate-900">Wert in DM: <br><input class="h-10 border mt-1 rounded px-4 w-fit bg-gray-50" type="number" name="Wert_DM"></label><br>
+    </div>
     <div class="flex-auto flex space-x-2">
       <input class="h-10 px-6 font-semibold rounded-md bg-blue-500 text-white" type="submit">
       <input class="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="reset">
@@ -31,7 +36,7 @@ export default {
   name: "MineralCreator",
   data () {
     return {
-      isVisible: true
+      isVisible: false
     }
   }
 }
